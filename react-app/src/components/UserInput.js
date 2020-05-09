6da177e5c44f146eedab158f7d4193f462058ad6
tstewart161 <1.61tstewart@gmail.com>
2020-05-09 15:33:39 -0400

    Added user input, fixing CORS error

--- react-app/src/components/UserInput.js
+++ react-app/src/components/UserInput.js
@@ -7,40 +7,59 @@ class UserInput extends React.Component {
 
         this.state = {
             search_term: '',
+            search_by: '',
+            tags: '',
             search_results: []
         }
-
-        this.handleChange = this.handleChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
     }
 
-    getSearchResults = (search_term) => {
+    getSearchResults = (sort_by, search_term, tags) => {
         // Gets search results based on input search term.
-        try {
+        // let url = 'http://hn.algolia.com/api/v1/' // Make sure this is error-free.
+        //         + sort_by
+        //         + `?query=${search_term}`
+        //         + `&tags=${tags}`; // This has to work with multiple tags AND/OR-ing
+        //         // What other params should I have?
+        let url = 'http://hn.algolia.com/api/v1/search?query=foo&tags=story';
 
-        } catch {
-
-        }
+        fetch(url).then((result) => {
+            console.log(result);
+        }).catch((error) => {
+            console.log(error);
+        })
     }
 
-    handleSubmit(event) {
-        // Handle search submit.
+    handleSubmit = (event) => {
+        event.preventDefault();
+        this.getSearchResults(this.state.sort_by, this.state.search_term, this.state.tags);
     }
 
-    handleChange(event) {
-        // Handle search change.
+    handleChange = (event) => {
+        this.setState({
+            [event.target.name]: event.target.value
+        })
     }
 
     render() {
         return (
             <div>
-                <form onSubmit={this.handleSubmit}>
-                    <label className="searchBar">
-                        <input type="text" name="search_term" onChange={this.handleChange}/>
+                <form onSubmit={this.handleSubmit} onChange={this.handleChange}>
+                    <label className="sortBy">
+                        Sort by:
+                        <input value="search" name="sort_by" type="radio"/>
+                        Relevance
+                        <input value="search_by_date" name="sort_by" type="radio"/>
+                        Date
                     </label>
-                    <div>
+                    <br/>
+                    <label className="searchTerm">
+                        Search term
+                        <input type="text" name="search_term"/>
+                    </label>
+                    <br/>
+                    <label className="submit">
                         <input type="submit" value="Search"/>
-                    </div>
+                    </label>
                 </form>
                 <Results search_results={this.state.search_results}/>
             </div>
